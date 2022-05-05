5ed1c4e2bef1add3c92e51c782e2f5fb0b647c6e
HoangPham <hoangpa@paditech.com>
2022-05-05 10:14:02 +0700

    Refactor menus

--- menu/index.js
+++ menu/index.js
@@ -1,11 +1,37 @@
 export default [
   {
-    id: 'dashboard',
-    name: 'home',
-    title: 'title.menu.home',
-    url: '/dashboard',
-    icon: 'icon-speedometer',
-    image: require('~/assets/less/images/home.png'),
-    access: '*'
+    name: 'Dashboard',
+    key: 'dashboard',
+    href: '/dashboard',
+    icon_black: '/assets/images/icon/icon1.png',
+    tablet_icon: '/assets/images/icon/icon1-mb.png'
+  },
+  {
+    name: 'Validators',
+    key: 'validators',
+    href: '/validators',
+    icon_black: '/assets/images/icon/icon2.png',
+    tablet_icon: '/assets/images/icon/icon2-mb.png'
+  },
+  {
+    name: 'Blocks',
+    key: 'blocks',
+    href: '/blocks',
+    icon_black: '/assets/images/icon/icon.png',
+    tablet_icon: '/assets/images/icon/icon-mb.png'
+  },
+  {
+    name: 'Transactions',
+    key: 'transactions',
+    href: '/transactions',
+    icon_black: '/assets/images/icon/icon3.png',
+    tablet_icon: '/assets/images/icon/icon3-mb.png'
+  },
+  {
+    name: 'Proposals',
+    key: 'proposals',
+    href: '/proposals',
+    icon_black: '/assets/images/icon/printer.png',
+    tablet_icon: '/assets/images/icon/printer-mb.png'
   }
 ]
